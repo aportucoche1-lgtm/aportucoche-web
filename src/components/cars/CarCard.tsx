ea05659741b27c21ec5710fbcbf60f51fb2df347
aportucoche1-lgtm <aportucoche1@gmail.com>
2026-04-24 10:58:29 +0200

    Update CarCard.tsx

--- src/components/cars/CarCard.tsx
+++ src/components/cars/CarCard.tsx
@@ -1,11 +1,4 @@
-import { getCarImage } from "../utils/getCarImage"
-import { ExternalLink, Fuel, Gauge, Heart, MapPin } from 'lucide-react';
 import { Car, AIValuation } from '../../types';
-import { AIBadge } from './AIBadge';
-import { BODY_TYPE_LABELS, FUEL_LABELS, PLATFORM_COLORS, PLATFORM_LABELS } from '../../lib/constants';
-import { useCarImage } from '../../hooks/useCarImage';
-
-const FALLBACK_IMAGE = 'https://images.unsplash.com/photo-fiEhVRWA5d0?auto=format&fit=crop&w=800&q=80';
 
 interface CarCardProps {
   car: Car;
@@ -16,22 +9,11 @@ interface CarCardProps {
   onAuthRequired: () => void;
 }
 
-export function CarCard({ car, valuation, isFavorite, onToggleFavorite, isLoggedIn, onAuthRequired }: CarCardProps) {
-  const imageUrl = useCarImage(car.brand, car.model, car.bodyType);
-
-  const handleFavorite = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (!isLoggedIn) { onAuthRequired(); return; }
-    onToggleFavorite(car);
-  };
-
-  const isChollo = valuation.rating === 'chollo_excelente';
-  const isBuen = valuation.rating === 'buen_precio';
-  const showSavings = isChollo || isBuen;
-
-return (
-  <div style={{ background: "blue", height: "300px", marginBottom: "20px" }}>
-    TARJETA VISIBLE
-  </div>
-);
+export function CarCard({ car }: CarCardProps) {
+  return (
+    <div style={{ background: "blue", height: "300px", marginBottom: "20px", color: "white", padding: "20px" }}>
+      TARJETA VISIBLE<br />
+      {car.brand} {car.model}
+    </div>
+  );
 }

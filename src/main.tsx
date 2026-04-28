659da4d994cbd0e694fd2b789d697ee4bde45231
aportucoche1-lgtm <aportucoche1@gmail.com>
2026-04-28 21:02:09 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+document.body.innerHTML = `
+  <div style="
+    background:white;
+    color:black;
+    font-size:40px;
+    padding:40px;
+    min-height:100vh;
+  ">
+    MAIN TSX FUNCIONANDO
+  </div>
+`;

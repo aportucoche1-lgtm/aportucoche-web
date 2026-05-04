7c5fc4c0874e867d6e6caa3eb3246b7920a7ebcd
aportucoche1-lgtm <aportucoche1@gmail.com>
2026-05-04 07:12:19 +0200

    Delete src/src/utils/src/utils/getCarImage.ts

--- src/src/utils/src/utils/getCarImage.ts
+++
@@ -1,5 +0,0 @@
-export function getCarImage(brand?: string, model?: string) {
-  const search = `${brand || ''} ${model || ''} car`.trim();
-
-  return `https://source.unsplash.com/1200x800/?${encodeURIComponent(search)}`;
-}
